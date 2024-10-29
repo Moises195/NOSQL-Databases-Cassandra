e450bbf0a33bd8ba3ab698f59d93b412dcf7c867
Moises195 <102815334+Moises195@users.noreply.github.com>
2024-10-29 10:26:53 -0500

    Create database.js

---
+++ database.js
@@ -0,0 +1,14 @@
+// database.js
+const cassandra = require('cassandra-driver');
+
+const client = new cassandra.Client({
+  contactPoints: ['127.0.0.1'],
+  localDataCenter: 'datacenter1',
+  keyspace: 'social_app'
+});
+
+client.connect()
+  .then(() => console.log('Connected to Cassandra'))
+  .catch(err => console.error('Failed to connect to Cassandra', err));
+
+module.exports = client;
